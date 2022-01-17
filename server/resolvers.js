8156640b9d3364a180191d7229d63c9dfbf93d34
vishnu-kyatannawar <vishnukyatannawar@gmail.com>
2022-01-18 00:04:56 +0530

    Add schema and resolver to delete a todo by todoId

--- server/resolvers.js
+++ server/resolvers.js
@@ -8,7 +8,15 @@ const Mutation = {
   create: (root, {description}) => {
      const todoID = todos.create({description});
      return todos.get(todoID);
-  }  
+  },
+  delete: (root, {id}) => {
+    const todo = todos.get(id);
+    if (todo) {
+      todos.delete(id);
+      return true;
+    }
+    return false;
+  }
 };
 
 export { Query, Mutation };
