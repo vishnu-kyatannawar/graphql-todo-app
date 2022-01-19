4a4c291bb6a4011bbe207b7516cc4b1b61350fcc
vishnu-kyatannawar <vishnukyatannawar@gmail.com>
2022-01-19 22:37:53 +0530

    Added a basic block to show todos

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,35 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container, Row, Col} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
+      <Container fluid className="main-container p-5">
+        <Row>
+          <Col>
+            <Container className="todo-wrapper">
+              <Row>
+                <Col></Col>
+                <Col xs={8}>
+                  <Container>
+                    <Row>
+                      <Col className="title-container">
+                          <h1>TODOs</h1>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col className="todo-box">
+                      </Col>
+                    </Row>
+                  </Container>
+                </Col>
+                <Col></Col>
+              </Row>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
